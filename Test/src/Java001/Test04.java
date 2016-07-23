4e932385061346b1d845a1c650cfecffa73bacb2
katsuhito.yamada <k-yamada@eiwa.com>
2016-07-23 15:54:03 +0900

    test04追加

---
+++ Test/src/Java001/Test04.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2016 EIS Co., Ltd. All rights reserved.
+ */
+package Java001;
+
+/**
+ * @author （作成者） <br />
+ *  （クラス論理名） <br />
+ *  （説明） <br />
+ *  更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test04 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * @param args 実行時引数３つ
+   */
+  public static void main( String[] args ) {
+    System.out.print(args[0]);
+    System.out.print(args[1]);
+    System.out.print(args[2]);
+  }
+
+}
